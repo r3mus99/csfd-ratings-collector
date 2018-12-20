b6217565f19a20f1ee2811df379f6ee7d4c1520c
r3mus99 <Jg9hqUELv56KrxC>
2018-12-20 09:50:30 +0100

    refactor

--- trieda.py
+++ trieda.py
@@ -1,34 +1,37 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
-
-url = "https://www.csfd.cz/uzivatel/51520-r3musko/hodnoceni/"
-soup = BeautifulSoup(requests.get(url).content, 'html.parser')
-
-table = soup.find("table")
-rows = table.find_all("tr")
-
-for row in rows:
-    cols = row.find_all("td")
-    if (cols):
-        nazov = cols[0].getText()
-        if cols[1].find("img") and cols[1].find("img").has_attr('alt'):
-            hodnotenie = cols[1].find("img").attrs['alt']
-        datum = cols[2].getText()
-        print(nazov + " [" + hodnotenie + "] " + datum)
-
 
 def get_next_page_url(nextPage):
     return url + "strana-" + str(nextPage) + "/"  # "https://www.csfd.cz/uzivatel/55555-marecko/hodnoceni/strana-2/"
 
+def print_table(table):
+    rows = table.find_all("tr")
 
-# check other sites
+    for row in rows:
+        cols = row.find_all("td")
+        if (cols):
+            nazov = cols[0].getText()
+            hodnotenie = ''
+            if cols[1].find("img") and cols[1].find("img").has_attr('alt'):
+                hodnotenie = cols[1].find("img").attrs['alt']
+            datum = cols[2].getText()
+            print(nazov + " [" + hodnotenie + "] " + datum)
+
+# print pages
+url = "https://www.csfd.cz/uzivatel/51520-r3musko/hodnoceni/"
+soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+
+table = soup.find("table")
 actualPage = 1
+
 while table is not None:
+    print_table(table)
+
     actualPage = actualPage + 1
     nextPageUrl = get_next_page_url(actualPage)
     nextPageRequest = requests.get(nextPageUrl)
 
     soup = BeautifulSoup(nextPageRequest.content, 'html.parser')
     table = soup.find("table")
-
     print("{0} - {1}".format(table is not None, nextPageUrl))
